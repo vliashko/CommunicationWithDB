b1ad371643b84f33bb6d08ab66c481753c89b03c
Vladzimir Liashko <vladzimir.liashko@gmail.com>
2022-05-24 15:47:45 +0300

    Correct comment for ADO, add implementation for dapper

---
+++ CommunicationWithDB.Dapper/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace CommunicationWithDB.Dapper
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public int Course { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+}
