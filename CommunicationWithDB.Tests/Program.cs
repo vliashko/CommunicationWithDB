b0b1414dfb62e8257f21d6587d4232a8b382cf75
Vladzimir Liashko <vladzimir.liashko@gmail.com>
2022-05-24 19:04:37 +0300

    Add benchmark for get all

---
+++ CommunicationWithDB.Tests/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BenchmarkDotNet.Running;
+using CommunicationWithDB.Tests;
+
+Console.WriteLine("Hello, World!");
+
+
+BenchmarkRunner.Run<Benchmark>();
