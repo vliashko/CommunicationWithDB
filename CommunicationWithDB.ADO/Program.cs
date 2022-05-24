a0ee39599b53704c805e9c24b2e1d9ee3643b8a8
Vladzimir Liashko <vladzimir.liashko@gmail.com>
2022-05-24 10:44:53 +0300

    Some translation changes / add params examples

--- CommunicationWithDB.ADO/Program.cs
+++ CommunicationWithDB.ADO/Program.cs
@@ -12,7 +12,7 @@ if (string.IsNullOrEmpty(connectionString))
     throw new ArgumentNullException(connectionString);
 }
 
-Console.WriteLine($"Connection string for ADO is: {connectionString}");
+Console.WriteLine($"Строка подключения для ADO: {connectionString}");
 
 // Объявлем соединение с определенной строкой подключения.
 var sqlConnection = new SqlConnection(connectionString);
@@ -20,7 +20,7 @@ var sqlConnection = new SqlConnection(connectionString);
 try
 {
     sqlConnection.Open();
-    Console.WriteLine("SQL Connection open.");
+    Console.WriteLine("SQL соединение открыто.");
 
     // Добавление (аналогичный код для обновления / удаления
     var sqlCommand = sqlConnection.CreateCommand();
@@ -54,6 +54,29 @@ try
     sqlCommandForCount.CommandText = "SELECT COUNT(*) FROM Student";
     var count = sqlCommandForCount.ExecuteScalar();
     Console.WriteLine($"Полное число студентов: {count}");
+
+    // Есть два варианта параметризации запросов
+    var name = "Some Student Name";
+
+    // Плохой вариант, так как позволяет получать и изменять данные при помощи механизма SQL-инъекций.
+    var sqlString = $"INSERT INTO Student VALUES ('{name}', 1, '20220101')";
+
+    var sqlCommandForInsertBadPractice = new SqlCommand(sqlString)
+    {
+        Connection = sqlConnection
+    };
+
+    affectedRows = sqlCommandForInsertBadPractice.ExecuteNonQuery();
+
+    // Хороший вариант, добавление SQL параметров
+    sqlString = $"INSERT INTO Student VALUES (@name, 1, '20220101')";
+    var sqlParamForName = new SqlParameter("@name", name);
+    var sqlCommandForInsertGoodPractice = new SqlCommand(sqlString);
+
+    // Добавление параметра
+    sqlCommandForInsertGoodPractice.Parameters.Add(sqlParamForName);
+
+    affectedRows = sqlCommandForInsertBadPractice.ExecuteNonQuery();
 }
 catch (Exception ex)
 {
